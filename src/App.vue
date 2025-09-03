d3ee53cc5fdc7024eae9f71b26728e3564f06b50
Maddax10 <maximilien01993@gmail.com>
2025-09-03 22:21:54 +0200

    feat: add routing and login functionality
    - Updated package.json to include sass-embedded for SCSS support.
    - Modified App.vue to include navigation links and router view.
    - Implemented router in index.js with routes for home and login views.
    - Created HomeView.vue as a placeholder for the home page.
    - Developed LoginView.vue with a login form and user authentication logic.
    - Enhanced vite.config.js to enable source maps for better debugging.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <nav>
+    <router-link class="link" to="/">home</router-link>
+    <router-link class="link" to="/login">login</router-link>
+  </nav>
+  <router-view></router-view>
 </template>
 
-<style scoped></style>
+<script setup>
+import { RouterView } from 'vue-router';
+
+</script>
+
+<style lang="scss" scoped>
+nav {
+  background-color: #212121;
+  display: flex;
+  gap: .5rem;
+
+  .link {
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      color: #ff3e3e;
+    }
+  }
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,76 @@
+<template>
+    <main>
+        <div class="login_card" v-if="userData === null">
+            <form action="">
+                <label for="">username</label>
+                <input type="text" v-model="usernameInput" />
+                <label for="">password</label>
+                <input type="text" v-model="passwordInput" />
+                <button @click.prevent="login">login</button>
+            </form>
+        </div>
+        <div class="connected" v-else>
+            <label>Vous êtes connectés !</label>
+            <button @click.prevent="disconnect">se deconnecter</button>
+        </div>
+    </main>
+</template>
+
+
+
+<script setup>
+import { ref } from 'vue'
+
+const usernameInput = ref('bob')
+const passwordInput = ref('passbob')
+
+let userData = ref(null)
+
+/**
+ * En 2 étapes:
+ *      1) Vérifier que l'username et le mdp existe bien
+ *      2) Dire qu'il est connecté
+ * Donc c'est sur le serveur API que je dois faire ça,
+ * une fois l'update fait, je renvoit l'objet qui à été modifié
+ */
+const login = async () => {
+    //1) Vérifier que l'username et le mdp existe bien, réponse d'un tableau vide s'il n'existe pas
+    userData.value = await connexion()
+    // console.log(data);
+    // console.log("data[0][\"_id_user\"]", data[0]["_id_user"]);
+    //2) dire qu'il est connecté
+    console.log(userData.value)
+}
+
+const connexion = async () => {
+    // console.log(`http://localhost:3000/users_EP/login/${usernameInput.value}/${passwordInput.value}`)
+    const resp = await fetch(
+        `http://localhost:3000/users_EP/login/connect/${usernameInput.value}/${passwordInput.value}`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+    )
+    // .then(resp => resp.json())
+    // .then(resp => {
+    //     console.log(resp)
+    //     if (resp.length === 0) {
+    //         console.log("Username or password is not correct !")
+    //     }
+    // })
+    if (resp.ok) {
+        const data = await resp.json()
+        return data
+    }
+
+    return null
+}
+
+const disconnect = () => {
+    userData.value = null;
+    console.log("disconnect");
+
+}
+</script>
