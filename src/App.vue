15e543411e28ddf8e6286d08b1016d3e38dd1d60
Max <maximilien01993@gmail.com>
2025-09-04 11:10:50 +0200

    refactor App.vue to conditionally render navigation links based on user connection status

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <nav>
-    <router-link class="link" to="/">login</router-link>
-    <router-link class="link" to="/register">register</router-link>
+    <router-link class="link" to="/" v-if="!usersStore.getUserConnexionInfo">login</router-link>
+    <router-link class="link" to="/register" v-if="!usersStore.getUserConnexionInfo">register</router-link>
+    <button class="link disconnect" @click.prevent="disconnect" v-if="usersStore.getUserConnexionInfo">se
+      deconnecter</button>
   </nav>
   <router-view></router-view>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import { useUsersStore } from './stores/users';
+
+const usersStore = useUsersStore();
+
+const disconnect = () => {
+  usersStore.disconnect();
+}
 
 </script>
 <style lang="scss" scoped>
@@ -24,11 +33,13 @@ nav {
     color: white;
     text-decoration: none;
     font-weight: 600;
+
     font-size: 1rem;
     padding: 0.75rem 1.5rem;
     border-radius: 8px;
     transition: all 0.3s ease;
     text-transform: capitalize;
+    cursor: pointer;
 
     &:hover {
       background: linear-gradient(135deg, rgba(255, 255, 255, 0.2) 0%, rgba(96, 96, 96, 0.2)100%);
@@ -46,5 +57,8 @@ nav {
       box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
     }
   }
+
+  .disconnect {
+    background-color: red; }
 }
 </style>
